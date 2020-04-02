ddf35b5a5c61e42d961e39a3189942ad5f2f6264
Yoshida Rick <chikara.f.yoshida@gmail.com>
2020-04-02 17:04:52 +0900

    支援センターで入室に成功 出力する内容を整理し,tkinterか何かで成功などを表示するようにしたい

--- form_request.py
+++ form_request.py
@@ -5,6 +5,12 @@ from get_wifi_SSID import WiFiUtil
 import time
 import datetime
 
+'''
+時間の出力：main()と例外時に出力
+'''
+date_now, time_now = datetime.datetime.now().split(' ')
+year, month, date = date_now.split('-')
+hour, minuits, second = time_now.split('-')
 wifi_SSID = ''
 counter = 0
 while wifi_SSID == '':
@@ -16,24 +22,21 @@ while wifi_SSID == '':
 
 
 def main():
-    fname = "exit.json"
-    with open(fname, "r") as f:
+    # fname = "entry.json" <- 起動時にファイル名指定だと ファイルが見つからないと言われるのでフルパス指定
+    with open('/Users/yoshidachikara/PycharmProjects/slack_bot/entry.json', "r") as f:
         entry = json.load(f)
         params = {"entry.{}".format(entry["entry"][k]): entry["output"][k] for k in entry["entry"].keys()}
         res = requests.get(entry["form_url"] + "formResponse", params=params)
-    if res.status_code == 200:
-        print("Done!")
-    else:
-        res.raise_for_status()
-        print("Error")
-    print(res.url)
+    with open('/Users/yoshidachikara/PycharmProjects/slack_bot/SSID.txt', mode="w") as f:
+        f.write(f"{year}年{month}月{date}日{time}時{minuits}分\n入室ボタンを押しました！\nSSIDは{wifi_SSID}です！")
+
+
 
 
 if __name__ == '__main__':
     if wifi_SSID in ('kigyoshimin@member', 'kigyoshimin@guest'):
         main()
     else:
-        with open('SSID.txt', mode="w") as f:
-            f.write(f'ssidは{wifi_SSID}です\n時間は{datetime.datetime.now()}です')
-
-        sys.exit()
+        with open('/Users/yoshidachikara/PycharmProjects/slack_bot/SSID.txt', mode="w") as f:
+            f.write(f'ssidは{wifi_SSID}です\n時間は{year}年{month}月{date}日{time}時{minuits}分です')
+    sys.exit()
